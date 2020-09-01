dedadd11ff4af87c0414676f64874034759be7f5
eoinlynchcodes <eoin95lynch@gmail.com>
2020-09-01 10:51:09 +0100

    Go

--- website/src/Containers/RightContainer.js
+++ website/src/Containers/RightContainer.js
@@ -10,6 +10,7 @@ function RightContainer() {
         <div className="rightBlackSegment">
           <h3>About</h3>
         </div>
+
         <div className="rightWhiteSegment">
           <p className="inlineParagraph">
             4 Hands Food Studio are producers of naturally fermented foods and
@@ -28,16 +29,64 @@ function RightContainer() {
             2018, in Rathaspick, Ireland.
           </p>
         </div>
+
         <div className="divWithRoseAndMargaux">
           <div className="margauxOrRoseDiv">
-            <img src={margaux} alt=""/>
-            <p>Margaux Dejardin. <br/>
-            Co-Founder of 4 Hands Food Studio.</p>          </div>
-          <div className="margauxOrRoseDiv">
-            <img src={rose} alt=""/>
-            <p>Rose Greene. <br/>
-            Co-Founder, 4 Hands Food Studio.</p>
+            <img src={margaux} alt="" />
+            <p>
+              Margaux Dejardin. <br />
+              Co-Founder of 4 Hands Food Studio.
+            </p>{" "}
           </div>
+          <div className="margauxOrRoseDiv">
+            <img src={rose} alt="" />
+            <p>
+              Rose Greene. <br />
+              Co-Founder, 4 Hands Food Studio.
+            </p>
+          </div>
+        </div>
+
+        <div className="rightContainer">
+          <div className="rightBlackSegment">
+            <h3>Our Principles</h3>
+          </div>
+
+          <ul>
+            <li>1. Make the best product possible.</li> <hr />
+            <li>
+              2. Always strive to improve and innovate on every level.
+            </li>{" "}
+            <hr />
+            <li>3. Nutrition is key.</li> <hr />
+            <li>
+              4. Connect with suppliers, continually control and monitor supply
+              chain.
+            </li>{" "}
+            <hr />
+            <li>
+              5. Minimize the creation of waste.
+            </li>{" "}
+            <hr />
+            <li>
+              6. Assess the environmental outcome of any business decision
+              before making it.
+            </li>{" "}
+            <hr />
+            <li>
+              7. Support and promote farmers that fit principles.
+            </li>{" "}
+            <hr />
+            <li>
+              8. Engage with and teach food citizens (customers).
+            </li><hr />
+            <li>
+              9. Continually push for the collective good, to give better access
+              to sustainable food to a maximum of people, while supporting
+              biodiversity and regenerative agriculture.
+            </li><hr />
+            <li>10. Lead by example.</li>
+          </ul>
         </div>
       </div>
       <h1>RightContainer</h1>
